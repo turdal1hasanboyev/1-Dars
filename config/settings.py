fa5fa3eaec154c9f4f315b9fc0237efc5cfd6858
turdal1hasanboyev <turdalihasanboyev70.com>
2024-06-07 17:07:06 +0500

    Finish Project!

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,9 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'django-insecure-mrrdarqe*j-wrc3c!^az=05n=z5gd!b*l3cpf%5dl)gak)b9v%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
